0a6ff96a049f0b2e0e110a5c5074ba75d87a7224
kicharr <99511093+kicharr@users.noreply.github.com>
2023-02-27 03:11:20 +0300

    add new pages

--- item1/js/main.js
+++ item1/js/main.js
@@ -3,7 +3,7 @@
 function onEntry(entry) {
     entry.forEach(change => {
         if (change.isIntersecting) {
-            change.target.classList.add('middle');
+            change.target.classList.add('showAdvice');
         }
     });
 }
@@ -17,3 +17,36 @@ let elements = document.querySelectorAll('.separate-advice');
 for (let elm of elements) {
     observer.observe(elm);
 }
+
+
+
+
+document.getElementById("showSearchBtn").addEventListener("click", showSearchBlock);
+
+function showSearchBlock() {
+    let searchBlock = document.getElementById('search-action__wrapper');
+    if (!searchBlock.style.cssText || searchBlock.style.cssText === "display: none;") {
+        searchBlock.style.cssText = " display: flex;"
+    } else {
+        searchBlock.style.cssText = " display: none;"
+    }
+}
+
+$(function () {
+    let header = $('.header');
+    let hederHeight = header.height(); // вычисляем высоту шапки
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1) {
+            header.addClass('header_fixed');
+            $('body').css({
+                'paddingTop': hederHeight + '100px' // делаем отступ у body, равный высоте шапки
+            });
+        } else {
+            header.removeClass('header_fixed');
+            $('body').css({
+                'paddingTop': 100 // удаляю отступ у body, равный высоте шапки
+            })
+        }
+    });
+});
